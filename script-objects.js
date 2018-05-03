92ff2debfc8ea1306df671b8487c32190c61e794
Radames Ajna <radamajna@gmail.com>
2018-05-03 16:25:33 -0700

    objects assignment

---
+++ script-objects.js
@@ -0,0 +1,30 @@
+
+/* key concepts
+
+  variables and functions -> become called properties and methods
+  dot notation, how to access the properties and methods
+  bracket notation also works person['name']
+
+*/
+
+/* hints
+
+  find element by id: document.querySelector("#id"), add ids to elements that are not yet identified
+  Image html tag:  <img src="smiley.gif" alt="Smiley face" class="img-fluid">,
+  element.innerHTML = replace with something
+
+*/
+
+//here is the start, defining and initializing the variable person.
+//fill up your dictionary with items
+var person = {};
+// identify all your elements you want to modify on your HTML page
+// select them and replace with the content of your object
+function start(){
+  //find the elements on your HTML page
+  var headBox = document.querySelector("#person-head");
+  //...
+  var infoName = document.querySelector("#info-name");
+  //....
+
+}
